209ca1fa6c1ca0aa7fb8203f48ce0342a695b546
elnaxy12 <aryagilang533@gmail.com>
2025-10-08 23:29:24 +0700

    git init

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -155,7 +155,7 @@
                     <div class="col-2">
                         <div class="row-1">
                             <div class="card paid-invoice">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
                                     class="lucide lucide-wallet-icon lucide-wallet">
@@ -166,7 +166,7 @@
 
                             </div>
                             <div class="card funds-received">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
                                     class="lucide lucide-banknote-arrow-up-icon lucide-banknote-arrow-up">
