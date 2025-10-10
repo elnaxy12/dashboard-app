908d2206e7bdf84432a606a36d4588d284e5af1c
elnaxy12 <aryagilang533@gmail.com>
2025-10-10 22:50:24 +0700

    git init

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard-App</title>
+    <title>Dashboard - App Business</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
